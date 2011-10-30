02eab1690e4d3927fb3fa5a10dfb39a373e5cdd9
Yann Sionneau <yann@minet.net>
2011-10-31 00:12:54 +0100

    code cleanup

--- examples/pipeline/pipeline.v
+++ examples/pipeline/pipeline.v
@@ -3,11 +3,13 @@ module pipeline(input clk, input reset, output DOR, input DIR, input ack_to_C, o
 wire DOR_A; /* Data Out Ready */
 wire DOR_B;
 
-wire DIR_B;
+wire DIR_B; /* Data In Ready */
 wire DIR_C;
 
-wire ack_from_B_to_A;
-wire ack_from_C_to_B;
+wire ack_to_A;
+wire ack_to_B;
+wire ack_from_B;
+wire ack_from_C;
 
 wire [7:0] data_out_A;
 wire [7:0] data_out_B;
@@ -21,8 +23,11 @@ assign DIR_C = DOR_B;
 assign data_in_B = data_out_A;
 assign data_in_C = data_out_B;
 
-stage_A sA (clk, reset, DOR_A, DIR, ack_from_B_to_A, ack_from_A, data_in, data_out_A);
-stage_B sB (clk, reset, DOR_B, DIR_B, ack_from_C_to_B, ack_from_B_to_A, data_in_B, data_out_B);
-stage_C sC (clk, reset, DOR, DIR_C, ack_to_C, ack_from_C_to_B, data_in_C, data_out);
+assign ack_to_A = ack_from_B;
+assign ack_to_B = ack_from_C;
+
+stage_A sA (clk, reset, DOR_A, DIR, ack_to_A, ack_from_A, data_in, data_out_A);
+stage_B sB (clk, reset, DOR_B, DIR_B, ack_to_B, ack_from_B, data_in_B, data_out_B);
+stage_C sC (clk, reset, DOR, DIR_C, ack_to_C, ack_from_C, data_in_C, data_out);
 
 endmodule
