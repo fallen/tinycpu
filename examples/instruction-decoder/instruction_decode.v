9d540bbc494e886f69953a3bd85091eaebb942bb
Yann Sionneau <yann@minet.net>
2012-01-08 22:57:53 +0100

    Add support for instruction SRA

--- examples/instruction-decoder/instruction_decode.v
+++ examples/instruction-decoder/instruction_decode.v
@@ -27,7 +27,8 @@ parameter INST_NOR = 7'd11;
 parameter INST_SLT = 7'd12;
 parameter INST_SLL = 7'd13;
 parameter INST_SRL = 7'd14;
-parameter WRITE_BACK_TO_D = 7'd15;
+parameter INST_SRA = 7'd15;
+parameter WRITE_BACK_TO_D = 7'd16;
 
 reg [6:0] state = IDLE;
 
@@ -119,6 +120,13 @@ begin
 					begin
 						$display("We decode instruction : srl");
 						instruction_state <= INST_SRL;
+					end
+
+					/* sra $d,$t,shamt*/
+					6'd3:
+					begin
+						$display("We decode instruction : sra");
+						instruction_state <= INST_SRA;
 					end
 
 					/* add $d,$s,$t */
@@ -398,6 +406,13 @@ begin
 		begin
 			$display("We execute srl $d, %d, %d", T, instruction[10:6]);
 			D <= T >> instruction[10:6];
+			state <= WRITE_BACK_TO_D;
+		end
+
+		INST_SRA:
+		begin
+			$display("We execute sra $d, %d, %d", T, instruction[10:6]);
+			D <= ~(~T >> instruction[10:6]);
 			state <= WRITE_BACK_TO_D;
 		end
 
